85fa8976349147c6565631c2649bc3e0bb8f49a3
elgav04 <eliasgavarrete4@gmail.com>
2025-11-27 00:39:12 -0600

    conceder permisos formemp 271125

--- src/constants/permisos.ts
+++ src/constants/permisos.ts
@@ -1,5 +1,5 @@
 export const PERMISOS_MENU: Record<string, string[]> = {
-    Administrador: ["/areas", "/turnos", "/empleados", "/registros", "/usuarios_sistema"],
+    Administrador: ["/areas", "/turnos", "/empleados","/empleados/form_emp","/registros", "/usuarios_sistema"],
     Supervisor:    ["/registros", "/usuarios_sistema"],
-    RRHH:          ["/areas", "/turnos", "/empleados", "/registros", "/usuarios_sistema"],
+    RRHH:          ["/areas", "/turnos", "/empleados","/empleados/form_emp","/registros", "/usuarios_sistema"],
   };
